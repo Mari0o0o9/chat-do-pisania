a1bafd4deb63b6def72ffe4474ffbb75630fb27d
Mari0 <discordtako8@gmail.com>
2024-01-12 03:35:42 +0100

    modified:   css/styleChat.css 	modified:   php/chat.php

--- php/chat.php
+++ php/chat.php
@@ -14,29 +14,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/styleChat.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="shortcut icon" href="../zdjecia/favicon.ico" type="image/x-icon">
     <title>Chat</title>
 </head>
 <body>
-    <header>
-        Osoba z która piszesz :D
-    </header>
-    <section id="chat">
-        <div class="message other-message">   
-            wqewqewquegqwe   
-        </div>
-        <div class="message my-message">
-            sajkdsksadsadasdasdasasadasdsdasda
-        </div>
-        <div class="message other-message">
-            sajkdsksadsadasdasdasasadasdsdasda
-        </div>
-    </section>
-    <footer>
-        <input type="text" id="text" placeholder="Napisz wiadomość...">
-        <button id="send" class="material-symbols-outlined">
-            send
-        </button>
-    </footer>
+    <nav>
+        <h2>Cześć <?= $login?>!!!</h2>
+        <h3>Czaty:</h3>
+        <input type="text" name="" id="" placeholder="Szukaj...">
+    </nav>
+    <main>
+        <header>
+            Osoba z która piszesz :D
+        </header>
+        <section id="chat">
+            <div class="message other-message">   
+                wqewqewquegqwe   
+            </div>
+            <div class="message my-message">
+                sajkdsksadsadasdasdasasadasdsdasda
+            </div>
+            <div class="message other-message">
+                sajkdsksadsadasdasdasasadasdsdasda
+            </div>
+        </section>
+        <footer>
+            <input type="text" id="text" placeholder="Napisz wiadomość...">
+            <button id="send" class="material-symbols-outlined">
+                send
+            </button>
+        </footer>
+    </main>
 </body>
 <script src="../JavaScript/chat.js"></script>
 </html>
